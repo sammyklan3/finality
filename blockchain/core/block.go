b2ea07b4510e97b3e6cfb9cb1bf949e6f2956192
sammyklan3 <sammyklane7@gmail.com>
2025-08-04 13:56:39 +0300

    Core: Add NewBlock fucntion to cleanly initialize new blocks

--- blockchain/core/block.go
+++ blockchain/core/block.go
@@ -17,6 +17,7 @@ type Block struct {
 	Nonce        int
 }
 
+// CalculateHash computes the hash of the block based on its contents.
 func (b *Block) CalculateHash() string {
 	var txBuffer bytes.Buffer
 	enc := gob.NewEncoder(&txBuffer)
@@ -28,4 +29,16 @@ func (b *Block) CalculateHash() string {
 	data := fmt.Sprintf("%d%d%s%x%d", b.Index, b.Timestamp, b.PrevHash, txBuffer.Bytes(), b.Nonce)
 	hash := sha256.Sum256([]byte(data))
 	return hex.EncodeToString(hash[:])
+}
+
+// NewBlock creates a new block with the given parameters and calculates its hash.
+func NewBlock(index int, timestamp int64, transactions []Transaction, prevHash string) *Block {
+	block := &Block{
+		Index:        index,
+		Timestamp:    timestamp,
+		Transactions: transactions,
+		PrevHash:     prevHash,
+	}
+	block.Hash = block.CalculateHash()
+	return block
 }
